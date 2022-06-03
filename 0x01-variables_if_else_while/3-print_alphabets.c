988e7f211faf223447cf400879da50d450de8ea0
Nicholas Igunbor <nickoklaus5@gmail.com>
2022-06-03 14:50:45 -0700

    <"my sixth commit">

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: end program
+ */
+int main(void)
+{
+	char low;
+	char up;
+
+	for (low = 'a'; low <= 'z'; ++low)
+
+		printf("%c", low);
+
+	for (UP = 'A'; UP <= 'Z'; ++UP)
+		printf("%c", UP);
+
+	printf("\n");
+
+	return (0);
+}
