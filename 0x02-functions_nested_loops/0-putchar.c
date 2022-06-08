9c35f9542003490a75ea15e27251216c5b912ab1
Nicholas Igunbor <nickoklaus5@gmail.com>
2022-06-08 02:11:09 -0700

    <"my seventh commit">

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * main - check the code
  * Return: Always 0.
@@ -9,7 +9,7 @@ void print_alphabet(void)
 
 	for (c = 'a'; c <= 'z'; c++)
 	
-		_putchar(c);
+		_putchar();
 	
 	_putchar('\n');
 }
