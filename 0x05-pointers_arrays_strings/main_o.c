dcc9bc38dfd701fcf35dc6c0a3f15d99f2a59f59
Nicholas Igunbor <nickoklaus5@gmail.com>
2022-06-13 16:23:05 -0700

    <"my seventh commit">

---
+++ 0x05-pointers_arrays_strings/main_o.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void puts_half(char *);
+/**
+ * main - check the codde for Holdberton school students.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "Holberton!";
+	puts_half(str);
+	return (0);
+}
