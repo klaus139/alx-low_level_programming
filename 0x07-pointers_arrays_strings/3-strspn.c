d14c1967224ded9e6078663ed01c1a887fab1194
Nicholas Igunbor <nickoklaus5@gmail.com>
2022-06-21 09:10:19 -0700

    <"my eight commit">

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *strspn - gets the length of a prefix substring.
+ * _strspn - gets the length of a prefix substring.
  * @s: initial segment.
  * @accept: accepted bytes.
  * Return: the number of accepted bytes.
