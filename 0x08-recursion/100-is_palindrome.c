02112c9975933bf0bd888d10c89197c0ddf45592
Nicholas Igunbor <nickoklaus5@gmail.com>
2022-06-22 11:34:24 -0700

    <"my ninth commit">

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * s_len - calculates the lenth of a string.
+ * @s: original string
+ *
+ * Return: length of string.
+ */
+int s_len(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + s_len(s + 1));
+	}
+}
+/**
+ * pal - check if a string is a palindrome.
+ * @s: original string
+ * @i: initial point of recursion
+ * @len: length of string
+ * Return: 1 if is palidrome otherwise 0.
+ */
+int pal(char *s, int i, int len)
+{
+	if (*(s + i) == *(s + len - 1 - i) && i == (len / 2))
+	{
+		return (1);
+	}
+	else if (*(s + i) != *(s + len - 1 - i))
+	{
+		return (pal(s, i + 1, len));
+	}
+}
+/**
+ * is_palindrome - check if the string is a palidrome ex ala.
+ * @s: original string
+ *
+ * Return: 1 if is a alindrome otherwise 0.
+ */
+int is_palindrome(char *s)
+{
+	if (pal(s, 0, s_len(s)) == 1)
+		return (1);
+	else
+		return (0);
+}
