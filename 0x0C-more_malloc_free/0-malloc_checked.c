52691b2955b5ca6804b3c256cad79ff0835f5f74
Nicholas Igunbor <nickoklaus5@gmail.com>
2022-06-30 11:56:49 -0700

    <"new commit">

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/*
+ * malloc_checked - Entry point
+ * @b: input amount
+ * Return: pointer to the new memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *n;
+
+	n = malloc(b);
+
+	if (n == NULL)
+		exit(98);
+	return (n);
+}
