756a95a587db978581504959899ed001dd9f5631
Nicholas Igunbor <nickoklaus5@gmail.com>
2022-07-04 10:00:15 -0700

    <"dogs and bones">

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
-#ifndef _dog_h_
+#ifndef DOG_H
 
-#define _dog_h_
+#define DOG_H
+typedef struct dog dog_t;
 
 /**
  * struct dog - such structure of dog
@@ -14,17 +15,60 @@ typedef struct dog
 	char *name;
 	float *age;
 	char *owner;
-} dog_t;
+};
+/**
+ * _putchar - prints a character
+ * @c: character
+ * Return: c
+ */
+int _putchar(char c); /*prototype _putchar*/
+
+/*function that initializes a variable of type struct dog*/
+/**
+ * init_dog - prints a character
+ * @d: character
+ * @name: char
+ * @age: int
+ * @owner: char
+ * Return: c
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+/*funciton that initiates a variable of type strruct dog*/
+/**
+ * new_dog - prints a character
+ * @name: first memeber
+ * @age: second member
+ * @owner: third member
+ * Description: Longer description
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * free_dog - print a character
+ * @d: first memeber
+ * Description: Longer description
+ */
 
 
-
-void print_dog(struct dog *d);
+void print_dog(struct dog *d);/*function that prints a struct dog*/
+/**
+ * new_dog - prints a character
+ * @name: first memeber
+ * @age: second member
+ * @owner: third member
+ * Description: Longer description
+ * Return: Always 0.
+ */
 
 
 
 dog_t *new_dog(char *name, float age, char *owner);
-
+/**
+ * free_dog - prints a character
+ * @d: first member
+ * Description: Longer description
+ */
 
 
 void free_dog(dog_t *d);
