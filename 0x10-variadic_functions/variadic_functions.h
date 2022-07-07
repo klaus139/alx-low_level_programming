ddbfa8c65fa0be0a825a98b6216986e267d5fbf1
Nicholas Igunbor <nickoklaus5@gmail.com>
2022-07-07 07:05:47 -0700

    <"variadic">

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -14,6 +14,17 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
 
+/**
+ * struct format - Data type of format
+ * @op: format
+ * @f: function
+ */
+
+typedef struct format
+{
+	char *op;
+	void (*f)(va_list all);
+} f;
 
 
 #endif
