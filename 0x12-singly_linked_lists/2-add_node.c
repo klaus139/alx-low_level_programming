dd038d9416cc39e848b6c01bfb36c5d1349197ec
Nicholas Igunbor <nickoklaus5@gmail.com>
2022-07-14 05:42:05 -0700

    <"linked list">

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
-int _strleb_recursion(char *s);
+int _strlen_recursion(char *s);
 
 /**
  * add_node - adds a new node at the beginning of a list_t list.
  * @head: pointer to the first element of the list
  * @str: string to set in the new node
  * Return: address of the new element, or NULL if it failed
- */
+ **/
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
