2ae77d7e5c9dd4bdf29bc830bbe3d97897db1587
Nicholas Igunbor <nickoklaus5@gmail.com>
2022-07-23 01:47:24 -0700

    <"manipulation">

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,7 +4,7 @@
  * print_binary - prints the binary representation of a number
  * @n: number to print
  */
-void print_binary(unsigned longint n)
+void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
